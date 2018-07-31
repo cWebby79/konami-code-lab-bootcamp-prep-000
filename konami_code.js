df00e52692de6f5498263757242cfb719d861aa0
Chris Webster <cgw57@hotmail.com>
2018-07-31 14:43:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  
+  let index = 0;
+ 
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
